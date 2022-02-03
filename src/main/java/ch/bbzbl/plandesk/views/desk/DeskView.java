c0fcaf91b72624485e2712c17334d6f21ee2c89d
start.vaadin.com <no-reply@vaadin.com>
2022-02-03 09:11:31 +0000

    Generated project

---
+++ src/main/java/ch/bbzbl/plandesk/views/desk/DeskView.java
@@ -0,0 +1,34 @@
+package ch.bbzbl.plandesk.views.desk;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+@PWA(name = "PlanDesk", shortName = "PlanDesk", enableInstallPrompt = false)
+@Theme(themeFolder = "plandesk")
+@PageTitle("Desk")
+@Route(value = "")
+public class DeskView extends VerticalLayout {
+
+    public DeskView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
